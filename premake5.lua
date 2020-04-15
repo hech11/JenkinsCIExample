b65748d455d83515053476b24ce8e453e30b8eaf
NP <nkp1@hotmail.co.uk>
2020-04-15 20:38:52 +0100

    Added the init files

---
+++ premake5.lua
@@ -0,0 +1,19 @@
+workspace "HelloWorld"
+	configurations
+	{ "Debug", "Release" }
+
+project "HelloWorld"
+	location "HelloWorld"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir "bin/%{cfg.buildcfg}"
+
+	files {"HelloWorld/src/**.h", "HelloWorld/src/**.cpp"}
+
+	filter "configurations:Debug"
+	defines {"DEBUG"}
+	symbols "On"
+
+	filter "configurations:Release"
+	defines {"NDEBUG"}
+	symbols "On"
